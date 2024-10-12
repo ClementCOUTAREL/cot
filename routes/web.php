efde596980b2fcdda73df96dfaaf82b64b09dae7
ClementCOUTAREL <coutarel.clement.pro@gmail.com>
2024-10-12 15:11:45 -0500

    Created groups for articles and events routes; php resolver implemented

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('about', [AboutController::class, 'index'] )->name('about');
+Route::get('about', [AboutController::class, 'index'])->name('about');
 
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 
-Route::get('articles', [ArticlesController::class, 'index'])->name('articles.index');
+Route::prefix('articles')->group(
+    function () {
+        Route::get('/', [ArticlesController::class,'index'])->name('articles.index');
+    }
+);
 
-Route::get('events', [EventsController::class, 'index'])->name('events.index');
+Route::prefix('events')->group(
+    function () {
+        Route::get('events', [EventsController::class, 'index'])->name('events.index');
+    }
+);
